0c2701afb23cd6101f5137b7b08a03ec1f70d6b7
Magali Modugno <magalimodugno@gmail.com>
2020-06-02 11:32:58 -0300

    Agrego producto e item

---
+++ src/main/java/producto/Item.java
@@ -0,0 +1,23 @@
+package producto;
+
+public class Item {
+
+	
+	private Producto producto;
+	private int cantidad;
+	
+	public Item(Producto producto, int cantidad) {
+		this.producto = producto;
+		this.cantidad = cantidad;
+	}
+
+	public Producto getProducto() {
+		return producto;
+	}
+
+	public int getCantidad() {
+		return cantidad;
+	}
+	
+	
+}
---
+++ src/main/java/producto/Producto.java
@@ -0,0 +1,47 @@
+package producto;
+
+public class Producto {
+
+		private int codProducto;
+		private String nombre;
+		private String descripcion;
+		private float precio;
+		private TipoItem tipoProducto;
+		
+		
+		
+		public Producto(int codigo, String unNombre, String descripcion,float unPrecio, TipoItem tipo) {
+			this.nombre = unNombre;
+			this.precio = unPrecio;
+			this.descripcion = descripcion;
+			this.codProducto = codigo;
+			this.tipoProducto = tipo;
+			
+		}
+
+
+
+		public int getCodProducto() {
+			return codProducto;
+		}
+
+
+
+		public String getNombre() {
+			return nombre;
+		}
+
+
+
+		public float getPrecio() {
+			return precio;
+		}
+
+
+
+		public TipoItem getTipoProducto() {
+			return tipoProducto;
+		}
+
+
+}
