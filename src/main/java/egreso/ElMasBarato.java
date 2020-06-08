42dab4cede9ebadd0002d837d44959ef40b8bfa5
YoelIbarra <yoelibarra98@gmail.com>
2020-06-08 17:44:58 -0300

    CrearEgresoTest Ready

--- src/main/java/egreso/ElMasBarato.java
+++ src/main/java/egreso/ElMasBarato.java
@@ -8,25 +8,12 @@ import static java.util.stream.Collectors.toList;
 
 public class ElMasBarato implements CriterioSeleccion {
 
-	public Double elMenor(List<Double> t) {
-	
-    double min = t.get(0);
-    for (int i = 0; i < t.size(); i++) {
-        if (t.get(i) < min) {
-            min = t.get(i);
-        }
-    }
-    
-	return min;
-    
-	}
-
 	@Override
 	public Presupuesto seleccionar(OrdenDeCompra ordenDeCompra) {
 
 		List<Double> lista = ordenDeCompra.getPresupuestos().stream().map(Presupuesto::valorTotal).collect(toList());
 		Double menorPrecio = Collections.min(lista);
-		Presupuesto presu = ordenDeCompra.getPresupuestos().stream().filter(m -> m.valorTotal() == menorPrecio).findFirst().get();
+		Presupuesto presu = ordenDeCompra.getPresupuestos().stream().filter(m -> m.valorTotal().equals(menorPrecio)).collect(toList()).get(0);
 		return presu;
 		
 	}
