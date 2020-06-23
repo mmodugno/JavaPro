3bac64940ea2d259000dcc389592ae469f234f90
YoelIbarra <yoelibarra98@gmail.com>
2020-06-23 17:16:16 -0300

    Fixes Resultados

---
+++ src/main/java/egreso/InfoResultado.java
@@ -0,0 +1,7 @@
+package egreso;
+
+public enum InfoResultado {
+    InsuficienteCantidadPresupuestos,
+    ItemsNoCorrespondientes,
+    Correcto
+}
--- src/main/java/egreso/MotivoFalla.java
+++
@@ -1,6 +0,0 @@
-package egreso;
-
-public enum MotivoFalla {
-    InsuficienteCantidadPresupuestos,
-    ItemsNoCorrespondientes
-}
