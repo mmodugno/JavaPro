4b173f87c6e45142b8b9b91d82f963b85b19c4ca
Alejandro Gonzalez <alex@sceu.frba.utn.edu.ar>
2020-07-09 18:24:06 -0300

    se actualiza test CrearEgreso

--- src/test/java/egreso/CrearEgresoTest.java
+++ src/test/java/egreso/CrearEgresoTest.java
@@ -3,7 +3,8 @@ package egreso;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import organizacion.Organizacion;
+
+import organizacion.*;
 import producto.*;
 import producto.TipoItem;
 import usuarios.CreadorUsuario;
@@ -13,6 +14,7 @@ import usuarios.Usuario;
 import java.io.FileNotFoundException;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.Assert.fail;
@@ -26,13 +28,16 @@ public class CrearEgresoTest {
     Item item1 = new Item(p1, 1,0.00);
     Item item2 = new Item(p2,2,0.00);
 
-    CondicionValidacion condicionValidacion = new CondicionValidacion();
-    ElMasBarato elMasBarato = new ElMasBarato();
+//    CondicionValidacion condicionValidacion = new CondicionValidacion();
+//    ElMasBarato elMasBarato = new ElMasBarato();
     OrdenDeCompra ordenDeCompra;
-    Validador validador;
 
     //ORGANIZCION-USUARIO-PRESUPUESTO-MEDIO DE PAGO
-    Organizacion organizacion = new Organizacion();
+    Organizacion primerOrganizacion = new Organizacion();
+    
+    EntidadJuridica entidadJuridica = new EntidadJuridica("Web Social ONG", "Web Social", "90-61775331-4", 1143, 01, Collections.emptyList());
+	
+    
     CreadorUsuario userMaker = new CreadorUsuario();
     Presupuesto presupuesto1;
     Presupuesto presupuesto2;
@@ -40,82 +45,29 @@ public class CrearEgresoTest {
     
  
     @Before
-    public void init() throws ClassNotFoundException, FileNotFoundException, SQLException, CreationError {
-        ordenDeCompra = new OrdenDeCompra(1,5);
+    public void init() throws ClassNotFoundException, FileNotFoundException, SQLException, CreationError, CloneNotSupportedException {
+    	
+    	primerOrganizacion.agregarEntidad(entidadJuridica);
+    	
+    	ordenDeCompra = new OrdenDeCompra(1,5);
         ordenDeCompra.agregarItem(item1);
         ordenDeCompra.agregarItem(item2);
-        validador = new Validador(condicionValidacion, elMasBarato);
-        Usuario userAdmin = userMaker.crearUsuario("guidoAdmin", "pru3b@tesT", "admin",organizacion);
+        presupuesto1 = new Presupuesto(ordenDeCompra.getItems(),proveedor1,medioDePago);
+        presupuesto1.getItems().get(0).setPrecioUnitario(80.00);
+        presupuesto1.getItems().get(1).setPrecioUnitario(30.00);
+        ordenDeCompra.agregarPresupuesto(presupuesto1);
+        presupuesto1.setAceptado();
+        Usuario userAdmin = userMaker.crearUsuario("guidoAdmin", "pru3b@tesT", "admin",primerOrganizacion);
         ordenDeCompra.agregarRevisor(userAdmin);
     }
     
     @Test
-   
-    public void presupuestoYorden() throws CloneNotSupportedException {
+    public void crearEgreso() throws CloneNotSupportedException {
     	
-
-    	presupuesto1 = new Presupuesto(ordenDeCompra.getItems(),proveedor1,medioDePago);
+    	ordenDeCompra.cerrarOrden();
+    	primerOrganizacion.getEntidades().get(0).nuevoEgreso(ordenDeCompra); ; // Obtengo primera Entidad para agregarle los Egresos
     	
-    	Assert.assertTrue(condicionValidacion.presupuestoCorrecto(ordenDeCompra,presupuesto1));
+    	Assert.assertEquals(110.00, primerOrganizacion.getEntidades().get(0).getEgresos().get(0).valorTotal(), 0.1);
     }
     
-    //TODO terminar de revisar este test
-
-    @Test
-    public void validarConPresupuesto() throws ErrorDeValidacion, CloneNotSupportedException {
-
-
-        
-        presupuesto1 = new Presupuesto(ordenDeCompra.getItems(),proveedor1,medioDePago);
-        presupuesto1.getItems().get(0).setPrecioUnitario(10.00);
-        presupuesto1.getItems().get(1).setPrecioUnitario(10.00);
-
-        presupuesto2 = new Presupuesto(ordenDeCompra.getItems(),proveedor2,medioDePago);
-        presupuesto2.getItems().get(0).setPrecioUnitario(15.00);
-        presupuesto2.getItems().get(0).setPrecioUnitario(15.00);
-
- 
-        ordenDeCompra.agregarPresupuesto(presupuesto1);
-        ordenDeCompra.agregarPresupuesto(presupuesto2);
-
-        Assert.assertEquals(2,ordenDeCompra.getPresupuestos().size());
-        Assert.assertEquals(0, organizacion.getEgresos().size());
-        
-        validador.validarOrden(ordenDeCompra);
-        
-        Assert.assertEquals(1, organizacion.getEgresos().size());
-
-    }
-
-    @Test
-    public void seleccionoElMasBarato() throws ErrorDeValidacion, CloneNotSupportedException {
-
-        presupuesto1 = new Presupuesto(ordenDeCompra.getItems(),proveedor1,medioDePago);
-        presupuesto1.getItems().get(0).setPrecioUnitario(10.00);
-        presupuesto1.getItems().get(1).setPrecioUnitario(10.00);
-
-        presupuesto2 = new Presupuesto(ordenDeCompra.getItems(),proveedor2,medioDePago);
-        presupuesto2.getItems().get(0).setPrecioUnitario(7.00);
-        presupuesto2.getItems().get(0).setPrecioUnitario(6.00);
-
-        ordenDeCompra.agregarPresupuesto(presupuesto1);
-        ordenDeCompra.agregarPresupuesto(presupuesto2);
-
-        validador.validarOrden(ordenDeCompra);
-
-        Assert.assertEquals( presupuesto2, organizacion.getEgresos().get(0).getPresupuesto());
-
-
-
-    }
-
-    @Test
-    public void validacionMala() throws ErrorDeValidacion {
-
-        try {
-            validador.validarOrden(ordenDeCompra);
-            fail("Se esperaba la excepcion ErrorDeValidacion");
-        } catch (ErrorDeValidacion e) {
-        }
-    }
 }
