4fbbf2dc84256c6defad56e8d13fda6af96c5327
Alejandro Gonzalez <alex@sceu.frba.utn.edu.ar>
2020-07-09 14:16:14 -0300

    se quita Egresos de Organizacion

--- src/main/java/organizacion/Organizacion.java
+++ src/main/java/organizacion/Organizacion.java
@@ -9,26 +9,14 @@ public class Organizacion {
 	public Organizacion(){
 		super();
 		this.entidades = new ArrayList<EntidadJuridica>();
-		this.egresos = new ArrayList<Egreso>();
 		this.ordenesPendientes = new ArrayList<OrdenDeCompra>();
 	}
 	
-	
 	private List<EntidadJuridica> entidades;
-	private List<Egreso> egresos;
 	private List<OrdenDeCompra> ordenesPendientes;
-	
-	
-	public void nuevoEgreso(Egreso nuevoEgreso) {
-		egresos.add(nuevoEgreso);
-	}
 	
 	public void agregarEntidad(EntidadJuridica entidad) {
 		entidades.add(entidad);
-	}
-
-	public List<Egreso> getEgresos() {
-		return egresos;
 	}
 
 	public void sacarOrden(OrdenDeCompra ordenDeCompra) {
