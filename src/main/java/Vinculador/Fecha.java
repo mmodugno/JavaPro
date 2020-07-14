96fbfcd10b4b24f7045ed4e18ce23b63f1285fac
Alejandro Gonzalez <alex@sceu.frba.utn.edu.ar>
2020-07-14 02:21:47 -0300

    criterio Vinculación ordenado por Fecha

---
+++ src/main/java/Vinculador/Fecha.java
@@ -0,0 +1,39 @@
+package Vinculador;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import egreso.Egreso;
+import egreso.Ingreso;
+
+public class Fecha extends CriterioDeVinculacion {
+
+	public Fecha(List<CondicionObligatoria> condicionesObligatorias) {
+		super(condicionesObligatorias);
+	}
+	
+	@Override
+    public void vincular(List<Egreso> egresos, List<Ingreso> ingresos) {
+        ordenar(ingresos, egresos);
+        for(int i = 0; i<ingresos.size(); i++){
+            List<Integer> indexDestruir = new ArrayList<Integer>();
+            for(int j = 0; j<egresos.size(); j++){
+                if(pasaCondiciones(ingresos.get(i),egresos.get(j))){
+                    ingresos.get(i).asociarEgreso(egresos.get(j));
+                    indexDestruir.add(j);
+                }
+                for(int z = 0; z<indexDestruir.size(); z++)
+                {
+                    ingresos.remove(indexDestruir.get(z)-z);
+                }
+            }
+        }
+    }
+	
+	@Override
+    public void ordenar(List<Ingreso> ingresos, List<Egreso> egresos) {
+		egresos.sort(Comparator.comparing(Egreso::getFecha));
+		ingresos.sort(Comparator.comparing(Ingreso::getFecha));
+    }
+}
