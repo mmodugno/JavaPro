1f14fdeec43824681fd0c938f6df33f2793a0526
Alejandro Gonzalez <alex@sceu.frba.utn.edu.ar>
2020-07-14 03:31:36 -0300

    agrego criterio Mix

---
+++ src/main/java/Vinculador/Mix.java
@@ -0,0 +1,26 @@
+package Vinculador;
+
+import java.util.List;
+
+import egreso.Egreso;
+import egreso.Ingreso;
+
+public class Mix extends CriterioDeVinculacion {
+
+	public Mix(List<CondicionObligatoria> condicionesObligatorias) {
+		super(condicionesObligatorias);
+	}
+	
+	private List<CriterioDeVinculacion> criteriosVinculacion;
+	
+	@Override // Revisar si se debe buscar la lista de Egresos e Ingresos dentro del método
+    public void vincular(List<Egreso> egresos, List<Ingreso> ingresos) {
+		
+		criteriosVinculacion.stream().forEach(criterioVinculacion -> criterioVinculacion.vincular(egresos, ingresos));
+    }
+	
+	public void agregarCriterioVinculacion(CriterioDeVinculacion criterio) {
+		criteriosVinculacion.add(criterio);
+	}
+
+}
