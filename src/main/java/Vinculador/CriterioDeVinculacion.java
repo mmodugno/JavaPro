131c58d2edfa9ec9747fd37632718c0dd0a1be78
YoelIbarra <yoelibarra98@gmail.com>
2020-07-16 16:40:57 -0300

    Fixes
    Los ordenarValor van a CriterioVinculacion ya que lo usan dos criterios.
    el critero primeroEgreso esta bien o eso creo. Ahora creo el test

--- src/main/java/Vinculador/CriterioDeVinculacion.java
+++ src/main/java/Vinculador/CriterioDeVinculacion.java
@@ -16,10 +16,24 @@ public abstract class CriterioDeVinculacion {
 
     void vincular(List<Egreso> egresos, List<Ingreso> ingresos) {}
 
-    void ordenar(List<Ingreso> ingresos, List<Egreso> egresos){}
+    void ordenarValor(List<Ingreso> ingresos, List<Egreso> egresos) {
+        ingresos.sort((Ingreso unIngreso, Ingreso otroIngreso) -> {
+            return ordenarDouble(unIngreso.getMonto(),otroIngreso.getMonto());
+        });
+        egresos.sort((Egreso unEgreso, Egreso otroEgreso) -> {
+            return ordenarDouble(unEgreso.valorTotal(), otroEgreso.valorTotal());
+        });
+    }
 
     boolean pasaCondiciones(Ingreso ingreso, Egreso egreso){
 
         return condicionesObligatorias.stream().allMatch(c -> c.cumpleCondicion(ingreso, egreso));
+    }
+
+    int ordenarDouble(double primero,double segundo){
+        if (primero > segundo) return 1;
+        if (primero < segundo) return -1;
+        return 0;
+
     }
 }
--- src/main/java/Vinculador/PrimeroIngreso.java
+++ src/main/java/Vinculador/PrimeroIngreso.java
@@ -11,24 +11,10 @@ public class PrimeroIngreso extends CriterioDeVinculacion {
     public PrimeroIngreso(List<CondicionObligatoria> condicionesObligatorias) {
         super(condicionesObligatorias);
     }
-    
-    @Override
-    void ordenar(List<Ingreso> ingresos, List<Egreso> egresos) {
-        ingresos.sort((Ingreso unIngreso, Ingreso otroIngreso) -> {
-            if (unIngreso.getMonto() > otroIngreso.getMonto()) return 1;
-            if (unIngreso.getMonto() < otroIngreso.getMonto()) return -1;
-            return 0;
-        });
-        egresos.sort((Egreso unEgreso, Egreso otroEgreso) -> {
-        	if (unEgreso.valorTotal() > otroEgreso.valorTotal()) return 1;
-        	if (unEgreso.valorTotal() < otroEgreso.valorTotal()) return -1;
-        	return 0;
-        });
-    }
 
     @Override
     void vincular(List<Egreso> egresos, List<Ingreso> ingresos) {
-        ordenar(ingresos, egresos);
+        ordenarValor(ingresos, egresos);
         for(int i = 0; i<egresos.size(); i++){
             List<Integer> indexDestruir = new ArrayList<Integer>();
             for(int j = 0; j<ingresos.size(); j++){
