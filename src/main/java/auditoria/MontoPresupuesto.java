0889830c0e0b5fd04e6e101c46f6272feace7f6c
Alejandro Gonzalez <alex@sceu.frba.utn.edu.ar>
2020-07-17 16:38:35 -0300

    se agrega nombre y método para describir Condición de Validación

--- src/main/java/auditoria/MontoPresupuesto.java
+++ src/main/java/auditoria/MontoPresupuesto.java
@@ -3,10 +3,17 @@ package auditoria;
 import egreso.OrdenDeCompra;
 
 public class MontoPresupuesto implements CondicionValidacion {
-
+	
+	
+	String nombre = "Monto Presupuesto";
+	
 	@Override
 	public boolean validar(OrdenDeCompra ordenDeCompra) {
 		return ordenDeCompra.presupuestoAceptado().valorTotal() == ordenDeCompra.valorTotal();
+	}
+	
+	public String getNombre() {
+		return nombre;
 	}
 
 }
