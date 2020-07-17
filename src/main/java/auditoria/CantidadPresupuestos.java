4c15a4983860a67e2c872ade13e2f97e17fefd37
Alejandro Gonzalez <alex@sceu.frba.utn.edu.ar>
2020-07-17 16:34:03 -0300

    se agrega nombre y método para describir Condición de Validación

--- src/main/java/auditoria/CantidadPresupuestos.java
+++ src/main/java/auditoria/CantidadPresupuestos.java
@@ -4,11 +4,17 @@ import egreso.OrdenDeCompra;
 
 public class CantidadPresupuestos implements CondicionValidacion {
 
+	String nombre = "Cantidad de Presupuestos";
+	
 	@Override
 	public boolean validar(OrdenDeCompra ordenDeCompra) {
 		int cantidadPresupuestos = ordenDeCompra.getPresupuestos().size();
         
         return ordenDeCompra.getNecesitaPresupuesto() <= cantidadPresupuestos;
+	}
+	
+	public String getNombre() {
+		return nombre;
 	}
 
 }
