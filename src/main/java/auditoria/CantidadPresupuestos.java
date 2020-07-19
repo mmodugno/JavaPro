7293a585777ca22f8c8a5d0431bf5070f0d66ca7
Alejandro Gonzalez <alex@sceu.frba.utn.edu.ar>
2020-07-19 03:35:20 -0300

    se actualiza clase CantidadPresupuestos a la clase abstracta CondicionValidacion

--- src/main/java/auditoria/CantidadPresupuestos.java
+++ src/main/java/auditoria/CantidadPresupuestos.java
@@ -2,19 +2,37 @@ package auditoria;
 
 import egreso.OrdenDeCompra;
 
-public class CantidadPresupuestos implements CondicionValidacion {
+public class CantidadPresupuestos extends CondicionValidacion {
 
-	String nombre = "Cantidad de Presupuestos";
+	private int cantidadPresupuestos;
+	private int presupuestosRequeridos;
 	
-	@Override
-	public boolean validar(OrdenDeCompra ordenDeCompra) {
-		int cantidadPresupuestos = ordenDeCompra.getPresupuestos().size();
-        
-        return ordenDeCompra.getNecesitaPresupuesto() <= cantidadPresupuestos;
+	public boolean validar(OrdenDeCompra ordenDeCompra, Reporte reporte) {
+		nombre = "Validación de Cantidad de Presupuestos Requeridos";
+		boolean validacion;
+
+		this.cantidadPresupuestos = ordenDeCompra.getPresupuestos().size();
+        this.presupuestosRequeridos = ordenDeCompra.getNecesitaPresupuesto();
+        		
+		validacion = presupuestosRequeridos <= cantidadPresupuestos;
+		
+		reporte.resultadoValidacionPresupuestos(this, validacion);
+		
+        return validacion;
 	}
 	
 	public String getNombre() {
 		return nombre;
 	}
+
+	public int getCantidadPresupuestos() {
+		return cantidadPresupuestos;
+	}
+
+	public int getPresupuestosRequeridos() {
+		return presupuestosRequeridos;
+	}
+	
+	
 
 }
