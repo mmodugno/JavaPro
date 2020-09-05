7f053c5606fe2a5aa4d85483d4b419c5bcca12cb
YoelIbarra <yoelibarra98@gmail.com>
2020-09-05 11:41:29 -0300

    Auditoria listo
    /*atributos*/
    /*GETTERS*/
    /*SETTERS*/
    /*Algunos comentarios */
    
    Agregue todos los getters y setters, en caso de que alguno no vaya lo sacamos. Mejor que sobre y no que falte

--- src/main/java/auditoria/Validador.java
+++ src/main/java/auditoria/Validador.java
@@ -8,11 +8,38 @@ import egreso.OrdenDeCompra;
 
 public class Validador {
 
+	/*ATRIBUTOS*/
 	List<CondicionValidacion> condicionesValidacion;
 	Reporte reporteValidacion;
-	
 	private boolean resultadoValidacion;
-	
+
+
+	/*GETTERS*/
+
+	public List<CondicionValidacion> getCondicionesValidacion() {
+		return condicionesValidacion;
+	}
+	public Reporte getReporteValidacion() {
+		return reporteValidacion;
+	}
+	public boolean isResultadoValidacion() {
+		return resultadoValidacion;
+	}
+
+	/*SETTERS*/
+
+	public void setCondicionesValidacion(List<CondicionValidacion> condicionesValidacion) {
+		this.condicionesValidacion = condicionesValidacion;
+	}
+	public void setReporteValidacion(Reporte reporteValidacion) {
+		this.reporteValidacion = reporteValidacion;
+	}
+	public void setResultadoValidacion(boolean resultadoValidacion) {
+		this.resultadoValidacion = resultadoValidacion;
+	}
+
+	/* Adentro del constructor si se pueden realizar acciones, lo que no se puede en hibernate o le molesta un poco es
+	* que le pases atributos por paramentros */
 	public Validador() {
 		condicionesValidacion = new ArrayList<CondicionValidacion>();
 		reporteValidacion = new Reporte();
