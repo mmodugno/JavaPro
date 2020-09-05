7679a370fe7029e69c259c6bb14cd9e439de2a82
YoelIbarra <yoelibarra98@gmail.com>
2020-09-04 21:50:01 -0300

    Atributos/Getter/Setter

--- src/main/java/auditoria/CondicionValidacion.java
+++ src/main/java/auditoria/CondicionValidacion.java
@@ -3,12 +3,19 @@ package auditoria;
 import egreso.OrdenDeCompra;
 
 public abstract class CondicionValidacion {
-	
+
+	/*ATRIBUTOS*/
 	protected String nombre;
 	
 	public abstract boolean validar(OrdenDeCompra ordenDeCompra, Reporte reporte);
 
+	/*GETTERS*/
 	public String getNombre() {
 		return nombre;
+	}
+
+	/*SETTERS*/
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
 	}
 }
--- src/main/java/auditoria/MontoPresupuesto.java
+++ src/main/java/auditoria/MontoPresupuesto.java
@@ -4,9 +4,27 @@ import egreso.OrdenDeCompra;
 
 public class MontoPresupuesto extends CondicionValidacion {
 
+	/*ATRIBUTOS*/
 	private double montoCompra;
 	private double montoPresupuestoAceptado;
-	
+
+	/*GETTERS*/
+	public double getMontoCompra() {
+		return montoCompra;
+	}
+	public double getMontoPresupuestoAceptado() {
+		return montoPresupuestoAceptado;
+	}
+
+	/*SETTERS*/
+
+	public void setMontoCompra(double montoCompra) {
+		this.montoCompra = montoCompra;
+	}
+	public void setMontoPresupuestoAceptado(double montoPresupuestoAceptado) {
+		this.montoPresupuestoAceptado = montoPresupuestoAceptado;
+	}
+
 	public boolean validar(OrdenDeCompra ordenDeCompra, Reporte reporte) {
 		nombre = "Validación de Monto Compra y Presupuesto";
 		boolean validacion;
@@ -19,12 +37,6 @@ public class MontoPresupuesto extends CondicionValidacion {
 		return validacion;
 	}
 
-	public double getMontoCompra() {
-		return montoCompra;
-	}
 
-	public double getMontoPresupuestoAceptado() {
-		return montoPresupuestoAceptado;
-	}
 
 }
