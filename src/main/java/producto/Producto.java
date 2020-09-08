305918a85053bd4adaa1f77fad23f61ea9ac988a
YoelIbarra <yoelibarra98@gmail.com>
2020-09-08 19:09:56 -0300

    todo menos usuarios

--- src/main/java/producto/Producto.java
+++ src/main/java/producto/Producto.java
@@ -2,39 +2,52 @@ package producto;
 
 public class Producto {
 
-		private int codProducto;
-		private String nombre;
-		private String descripcion;
-		private TipoItem tipoProducto;
+
+	public Producto(int codigo, String unNombre, String descripcion, TipoItem tipo) {
+		this.nombre = unNombre;
+		this.descripcion = descripcion;
+		this.codProducto = codigo;
+		this.tipoProducto = tipo;
+
+	}
+	/*
+	* setNombre(nombre);
+	* setDescripcion(descripcion);
+	* setCodProducto(codProducto);
+	* setTipoProducto(tipoProducto);
+	*/
+
+	/*ATRIBUTOS*/
+	private int codProducto;
+	private String nombre;
+	private String descripcion;
+	private TipoItem tipoProducto;
 		
-		
-		
-		public Producto(int codigo, String unNombre, String descripcion, TipoItem tipo) {
-			this.nombre = unNombre;
-			this.descripcion = descripcion;
-			this.codProducto = codigo;
-			this.tipoProducto = tipo;
-			
-		}
+	/*GETTERS*/
+	public int getCodProducto() {
+		return codProducto;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public TipoItem getTipoProducto() {
+		return tipoProducto;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
 
-
-		public int getCodProducto() {
-			return codProducto;
-		}
-
-
-		public String getNombre() {
-			return nombre;
-		}
-
-
-		public TipoItem getTipoProducto() {
-			return tipoProducto;
-		}
-
-
-		public String getDescripcion() {
-			return descripcion;
-		}
-
+	/*SETTERS*/
+	public void setCodProducto(int codProducto) {
+		this.codProducto = codProducto;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public void setTipoProducto(TipoItem tipoProducto) {
+		this.tipoProducto = tipoProducto;
+	}
 }
--- src/main/java/producto/Proveedor.java
+++ src/main/java/producto/Proveedor.java
@@ -6,20 +6,44 @@ import java.util.List;
 
 public class Proveedor {
 
+
+	/*CONSTRUCTOR*/
 	public Proveedor(String nombre, String cuilOCuit, String direccionPostal) {
 		this.nombre = nombre;
 		this.cuilOCuit = cuilOCuit;
 		this.direccionPostal = direccionPostal;
 	}
 
+	/*
+	* setNombre(nombre);
+	* setCuilOCuit(cuil);
+	* setDireccionPostal(direccionPostal);
+	*/
+
+	/*ATRIBUTOS*/
 	private String nombre;
 	private String cuilOCuit;
 	private String direccionPostal;
 
+	/*GETTERS*/
 	public String getCuilOCuit() {
 		return cuilOCuit;
 	}
+	public String getNombre() {
+		return nombre;
+	}
+	public String getDireccionPostal() {
+		return direccionPostal;
+	}
 
-
-	
+	/*SETTERS*/
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public void setCuilOCuit(String cuilOCuit) {
+		this.cuilOCuit = cuilOCuit;
+	}
+	public void setDireccionPostal(String direccionPostal) {
+		this.direccionPostal = direccionPostal;
+	}
 }
