a71d2abf02a0265c6a389a42fdcd0c35e9879125
Alejandro Gonzalez <alexjg@hotmail.com.ar>
2020-10-13 00:42:22 -0300

    se actualiza método validarLogin

--- src/main/java/server/ControllerLogin.java
+++ src/main/java/server/ControllerLogin.java
@@ -1,8 +1,14 @@
 package server;
 
+import java.io.FileNotFoundException;
+import java.sql.SQLException;
+
+import repositorios.RepositorioUsuario;
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
+import usuarios.CreationError;
+import usuarios.Usuario;
 
 public class ControllerLogin {
 	
@@ -12,8 +18,36 @@ public class ControllerLogin {
         return new ModelAndView(null, "login.html");
     }
     
-    public ModelAndView validarLogin(Request request, Response response) {
-        return new ModelAndView(null, "login.html");
+    public ModelAndView validarLogin(Request request, Response response) throws FileNotFoundException, ClassNotFoundException, CreationError, SQLException {
+    	
+    	String user = request.queryParams("username");
+        String pass = request.queryParams("password");
+        
+        RepositorioUsuario repoUsuario = new RepositorioUsuario();
+    	
+    	//List<Usuario> usuarios = repoUsuario.todos();
+        //Usuario usuario = repoUsuario.checkUser(user, pass);
+        Usuario usuario = repoUsuario.buscarUsuario(user);
+
+        if(usuario == null) {
+	        response.status(401);
+	        response.redirect("/login/incorrecto");
+        }
+        
+        if((usuario.getNombre().equals(user)) && (usuario.getPassword().equals(pass))) {
+        	
+        	request.session(true);
+            request.session().attribute("user", user);
+            request.session().attribute("usuario", usuario);
+            request.session().maxInactiveInterval(3600);
+
+            response.redirect("/inicio");
+        }
+        
+        response.status(401);
+        response.redirect("/login/incorrecto");
+
+        return null;
     }
 
 }
