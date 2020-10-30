aa0223ba48f0ca5aba625ec6ce7775de58ace898
Alejandro Gonzalez <alexjg@hotmail.com.ar>
2020-10-30 04:56:51 -0300

    Update RepositorioUsuario.java
    Actualizo constructor

--- src/main/java/repositorios/RepositorioUsuario.java
+++ src/main/java/repositorios/RepositorioUsuario.java
@@ -6,23 +6,38 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityManager;
+
+import db.EntityManagerHelper;
 import organizacion.Organizacion;
 import usuarios.CreadorUsuario;
 import usuarios.CreationError;
 import usuarios.Usuario;
 
 public class RepositorioUsuario {
+	
+	private EntityManager entityManager;
+	
     static List<Usuario> usuarios = null;
 
-    public RepositorioUsuario() throws FileNotFoundException, ClassNotFoundException, CreationError, SQLException {
-        if (usuarios == null) {
+    public RepositorioUsuario(EntityManager entityManager) throws FileNotFoundException, ClassNotFoundException, CreationError, SQLException {
+        
+    	this.entityManager = entityManager;
+    	
+    	if (usuarios == null) {
+    		
         	
         	CreadorUsuario userMaker = new CreadorUsuario();
         	Organizacion organizacion = new Organizacion();
         	
-        	Usuario userStandard = userMaker.crearUsuario("userStandard", "pru3b@tesT", "estandar", organizacion); 
+        	Usuario userStandard = userMaker.crearUsuario("userStandard", "pru3b@tesT", "estandar", organizacion);
             usuarios = new ArrayList<>();
             usuarios.add(userStandard);
+            
+        	EntityManagerHelper.beginTransaction();
+		    EntityManagerHelper.getEntityManager().persist(userStandard);
+		    EntityManagerHelper.commit();
+		    
         }
     }
 
