e2a3c4f55145b34d3b2c8d70fbb19f8283f0e208
Alejandro Gonzalez <alexjg@hotmail.com.ar>
2020-11-22 05:12:37 -0300

    Create ControllerPresupuesto.java

---
+++ src/main/java/server/ControllerPresupuesto.java
@@ -0,0 +1,60 @@
+package server;
+
+import java.io.FileNotFoundException;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.persistence.EntityManager;
+
+import egreso.OrdenDeCompra;
+import egreso.Presupuesto;
+import repositorios.RepositorioOrdenDeCompra;
+import repositorios.RepositorioPresupuesto;
+import repositorios.RepositorioUsuario;
+import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
+import usuarios.CreationError;
+import usuarios.Usuario;
+
+public class ControllerPresupuesto {
+	
+	public ModelAndView presupuestos(Request request, Response response, EntityManager entityManager) throws CloneNotSupportedException {
+
+		if(request.session().attribute("user") == null) {
+			response.redirect("/login");
+			return new ModelAndView(null, "ingresos.html");
+		}
+	
+		RepositorioUsuario repoUser = null;
+		try {
+			repoUser = new RepositorioUsuario(entityManager);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (CreationError e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+		//Usuario userActual = repoUser.byNombre(request.session().attribute("user"));
+	    
+		RepositorioPresupuesto repo = new RepositorioPresupuesto(entityManager);
+	    List<Presupuesto> presupuestos = repo.todos();
+
+	    Map<String, Object> map = new HashMap<>();
+	    map.put("presupuestos", presupuestos);
+	    //map.put("usuario", request.session().attribute("user"));
+	
+	    return new ModelAndView(map, "presupuestos.html");
+	}
+
+}
