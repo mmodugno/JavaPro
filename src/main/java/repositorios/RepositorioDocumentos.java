77bc9f23996ba4bd6375bb85b56a8f1da10d411f
YoelIbarra <yoelibarra98@gmail.com>
2020-11-23 20:31:59 -0300

    Mongo+Morphia+Atlas

--- src/main/java/repositorios/RepositorioDocumentos.java
+++ src/main/java/repositorios/RepositorioDocumentos.java
@@ -3,6 +3,8 @@ package repositorios;
 
 import com.google.gson.Gson;
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoDatabase;
 import dev.morphia.Datastore;
 import dev.morphia.Morphia;
 import jdk.internal.org.objectweb.asm.util.TraceMethodVisitor;
@@ -17,16 +19,30 @@ public class RepositorioDocumentos {
     public void crearTransaccion(Transaccion transaccion){
         Morphia morphia = new Morphia();
         morphia.mapPackage("com.baeldung.morphia");
-        Datastore datastore = morphia.createDatastore(new MongoClient("localhost",27017), "GeSoc");
+        MongoClientURI uri = new MongoClientURI(
+                "mongodb://GeSoc:dds2020@cluster0-shard-00-00.lvoi9.mongodb.net:27017,cluster0-shard-00-01.lvoi9.mongodb.net:27017,cluster0-shard-00-02.lvoi9.mongodb.net:27017/GeSoc?ssl=true&replicaSet=atlas-r6t4sh-shard-0&authSource=admin&retryWrites=true&w=majority");
+
+        MongoClient mongoClient = new MongoClient(uri);
+
+
+        Datastore datastore = morphia.createDatastore(mongoClient, "GeSoc");
         datastore.ensureIndexes();
 
         datastore.save(transaccion);
     }
 
     public Object documentos(String tipo, String operacion) {
+
         Morphia morphia = new Morphia();
         morphia.mapPackage("com.baeldung.morphia");
-        Datastore datastore = morphia.createDatastore(new MongoClient("localhost",27017), "GeSoc");
+
+        MongoClientURI uri = new MongoClientURI(
+                "mongodb://GeSoc:dds2020@cluster0-shard-00-00.lvoi9.mongodb.net:27017,cluster0-shard-00-01.lvoi9.mongodb.net:27017,cluster0-shard-00-02.lvoi9.mongodb.net:27017/GeSoc?ssl=true&replicaSet=atlas-r6t4sh-shard-0&authSource=admin&retryWrites=true&w=majority");
+
+        MongoClient mongoClient = new MongoClient(uri);
+
+
+        Datastore datastore = morphia.createDatastore(mongoClient, "GeSoc");
         datastore.ensureIndexes();
         List<Transaccion> transacciones = new ArrayList<>();
 
--- src/test/java/Persitence/ContextTest.java
+++ src/test/java/Persitence/ContextTest.java
@@ -2,12 +2,23 @@ package Persitence;
 
 import static org.junit.Assert.*;
 
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoDatabase;
 import org.junit.Test;
 import org.uqbarproject.jpa.java8.extras.WithGlobalEntityManager;
 import org.uqbarproject.jpa.java8.extras.test.AbstractPersistenceTest;
 
 public class ContextTest extends AbstractPersistenceTest implements WithGlobalEntityManager {
+/*
+    @Test
+    public void contextUp() {
+        MongoClientURI uri = new MongoClientURI(
+                "mongodb://GeSoc:dds2020@cluster0-shard-00-00.lvoi9.mongodb.net:27017,cluster0-shard-00-01.lvoi9.mongodb.net:27017,cluster0-shard-00-02.lvoi9.mongodb.net:27017/test?ssl=true&replicaSet=atlas-r6t4sh-shard-0&authSource=admin&retryWrites=true&w=majority");
 
+        MongoClient mongoClient = new MongoClient(uri);
+        MongoDatabase database = mongoClient.getDatabase("test");
+    }*/
 /*	@Test
 	public void contextUp() {
 		assertNotNull(entityManager());
@@ -16,4 +27,6 @@ public class ContextTest extends AbstractPersistenceTest implements WithGlobalEn
 	public void contextUpWithTransaction() throws Exception {
 		withTransaction(() -> {});
 	}*/
+
+
 }
