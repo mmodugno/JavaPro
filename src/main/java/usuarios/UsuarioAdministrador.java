4b25b8cc4e331f057a339cdcfe7a22ebb939eda8
Alejandro Gonzalez <alexjg@hotmail.com.ar>
2020-11-24 02:04:11 -0300

    Agrego constructor por defecto a UsuarioAdministrador

--- src/main/java/usuarios/UsuarioAdministrador.java
+++ src/main/java/usuarios/UsuarioAdministrador.java
@@ -6,13 +6,19 @@ import java.io.FileNotFoundException;
 import java.sql.SQLException;
 import java.util.List;
 
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("Administrador")
 public class UsuarioAdministrador extends Usuario {
+	
+	public UsuarioAdministrador() {
+    }
 	
 	public UsuarioAdministrador(String nombreUser, String unaPass, Organizacion organizacion, boolean creadoConExito) throws FileNotFoundException, ClassNotFoundException, CreationError, SQLException {
 		super(nombreUser, unaPass, organizacion, creadoConExito);
 	}
-	
-
 	
 	public void crearCategoriaCompuesta(String nombreCategoria, String criterio, List<CategoriaDelSistema> lista) {
 		CategoriaCompuesta nuevaCategoria = new CategoriaCompuesta(nombreCategoria,criterio);
