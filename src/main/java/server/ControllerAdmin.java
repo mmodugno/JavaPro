9ddaae880acc61f7162429588f7514f8c782e6a2
Alejandro Gonzalez <alexjg@hotmail.com.ar>
2020-11-24 02:08:57 -0300

    Create ControllerAdmin.java

---
+++ src/main/java/server/ControllerAdmin.java
@@ -0,0 +1,27 @@
+package server;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
+
+public class ControllerAdmin {
+	
+	public ModelAndView index(Request request, Response response) {
+	    	
+		System.out.println("Valor Atributo Session admin : " + request.session().attribute("admin"));
+		
+		if((request.session().attribute("user") == null))
+    		response.redirect("/login");
+    	if((request.session().attribute("admin").equals(false)))
+    		response.redirect("/login");
+    	
+    	Map<String, Object> map = new HashMap<>();
+        map.put("usuario", request.session().attribute("user"));
+        
+        return new ModelAndView(map , "admin.html");
+    }
+
+}
